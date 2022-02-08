97b6367a418b6b0edce4bba5407d8ac562621a18
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2022-02-08 12:05:21 +0100

    animation menu bar

--- src/components/Hamburger/Hamburger.js
+++ src/components/Hamburger/Hamburger.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./hamburger.css";
-const Hamburger = () => {
+const Hamburger = ({ isOpen }) => {
   return (
     <>
       <div className="hamburger">
@@ -8,6 +8,34 @@ const Hamburger = () => {
         <div className="burger burger2" />
         <div className="burger burger3" />
       </div>
+      <style jsx>{`
+        .hamburger {
+          width: 2rem;
+          height: 2rem;
+          display: flex;
+          justify-content: space-around;
+          flex-flow: column nowrap;
+          z-index: 10;
+        }
+        .burger {
+          width: 2rem;
+          height: 0.25rem;
+          border-radius: 10px;
+          background-color: white;
+          transform-origin: 1px;
+          transition: all 0.3s linear;
+        }
+        .burger1 {
+          transform: ${isOpen ? "rotate(45deg)" : "rotate(0)"};
+        }
+        .burger2 {
+          transform: ${isOpen ? "translateX(100%)" : "translateX(0)"};
+          opacity: ${isOpen ? 0 : 1};
+        }
+        .burger3 {
+          transform: ${isOpen ? "rotate(-45deg)" : "rotate(0)"};
+        }
+      `}</style>
     </>
   );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../images/logo.png";
 import Hamburger from "./Hamburger/Hamburger";
+import "../App.css";
 const Navbar = () => {
+  const [hamburgerOpen, setHamburgerOpen] = useState(false);
+
+  const toggleHamburger = () => {
+    setHamburgerOpen(!hamburgerOpen);
+  };
   return (
     <nav className="navbar black navbar-expand-lg fixed-top navbar-dark header">
       <div className="container">
@@ -9,49 +15,48 @@ const Navbar = () => {
           <img src={logo} className="d-inline-block align-middle" alt="" />
           Andreia Spicy online Shop
         </a>
-        {/* <button className="navbar-toggler" type="button">
-          <span className="navbar-toggler-icon"></span>
-        </button> */}
-        <div
-          className="collapse navbar-collapse d-sm-0 d-md-0"
-          id="navbarSupportedContent"
-        >
-          <ul className="navbar-nav ml-auto nav-pills">
-            <li className="nav-item active">
-              <a className="nav-link" href=" ">
-                Home <span className="sr-only"></span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href=" ">
-                About
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href=" ">
-                Services
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href=" ">
-                Gallery
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href=" ">
-                Menu
-              </a>
-            </li>
+        <div className="navigation">
+          <div
+            className="collapse navbar-collapse d-sm-0 d-md-0"
+            id="navbarSupportedContent"
+          >
+            <ul className="navbar-nav ml-auto nav-pills">
+              <li className="nav-item active">
+                <a className="nav-link" href=" ">
+                  Home <span className="sr-only"></span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href=" ">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href=" ">
+                  Services
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href=" ">
+                  Gallery
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href=" ">
+                  Menu
+                </a>
+              </li>
 
-            <li className="nav-item">
-              <a className="nav-link" href=" ">
-                Contact
-              </a>
-            </li>
-          </ul>
-          <div className="hamburger">
-            <Hamburger />
+              <li className="nav-item">
+                <a className="nav-link" href=" ">
+                  Contact
+                </a>
+              </li>
+            </ul>
           </div>
+        </div>
+        <div className="menu-hamburger" onClick={toggleHamburger}>
+          <Hamburger isOpen={hamburgerOpen} />
         </div>
       </div>
     </nav>
